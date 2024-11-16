2746b5c8ee5ffd837f62e4205d5f04647112005f
Vincent4470 <vvincent672@gmail.com>
2024-11-16 14:39:18 +0700

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,6 +11,8 @@ use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -22,7 +24,7 @@ class AuthController extends Controller
             'name' => 'required|string',
             'email' => 'required|email',
             'password' => 'required|string|min:6',
-            'pin' => 'required|digits:6', 
+            'pin' => 'required|digits:6',
         ]);
 
         if ($validator->fails()) {
@@ -36,7 +38,7 @@ class AuthController extends Controller
         }
 
         DB::beginTransaction();
-        
+
         try {
             $profilePicture = null;
             $ktp = null;
@@ -69,34 +71,78 @@ class AuthController extends Controller
 
             DB::commit();
 
+            $token = JWTAuth::attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+            
+
+            $userResponse = getUser($request->email);
+            $userResponse->token = $token;
+            $userResponse->token_expires_in = JWTAuth::factory()->getTTL() * 60;
+            $userResponse->token_type = 'bearer';
+
+            return response()->json($userResponse);
         } catch (\Throwable $th) {
             DB::rollBack();
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }
 
-    private function generateCardNumber($length) 
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], 400);
+        }
+
+        try {
+            $token = JWTAuth::attempt($credentials);
+
+            if (!$token) {
+                return response()->json(['message' => 'Login Credentials Invalid']);
+            }
+
+            $userResponse = getUser($request->email);
+            $userResponse->token = $token;
+            $userResponse->token_expires_in = JWTAuth::factory()->getTTL() * 60;
+            $userResponse->token_type = 'bearer';
+
+            return response()->json($userResponse, 200);
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    private function generateCardNumber($length)
     {
         $result = '';
         for ($i = 0; $i < $length; $i++) {
             $result .= mt_rand(0, 9);
         }
-        
+
         $wallet = Wallet::where('card_number', $result)->exists();
 
         if ($wallet) {
             return $this->generateCardNumber($length);
-        } 
+        }
 
         return $result;
     }
 
-    private function uploadBase64Image($base64Image) {
+    private function uploadBase64Image($base64Image)
+    {
         $decoder = new Base64ImageDecoder($base64Image, $allowedFormats = ['jpeg', 'png', 'jpg']);
 
         $decodedContent = $decoder->getDecodedContent();
         $format = $decoder->getFormat();
-        $image = Str::random(10).'.'.$format;
+        $image = Str::random(10) . '.' . $format;
         Storage::disk('public')->put($image, $decodedContent);
 
         return $image;
